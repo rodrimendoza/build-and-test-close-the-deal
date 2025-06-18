1eeb3bfbda8b83692ab56817274051edad76357d
Rodrigo Mendoza <75888076+rodrimendoza@users.noreply.github.com>
2025-06-18 11:05:28 +0200

    Update string.test.js

--- tests/string.test.js
+++ tests/string.test.js
@@ -27,7 +27,7 @@ describe('String Utilities', () => {
   });
 
   // **Failing test**: remove me!
-  // trigger CI
+  // trigger CI 2
   test('countVowels should count the number of consonant in a string', () => {
     expect(countVowels('hello')).toBe(2);
   });
